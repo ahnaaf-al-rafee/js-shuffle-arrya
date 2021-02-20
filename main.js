dc56c97d79fd85fe89dc334c5b121820e8b8cc01
Ahnaaf Al Rafee <64587869+ahnaaf-al-rafee@users.noreply.github.com>
2021-02-20 13:36:35 +0600

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const unshuffled = ['hello', 'a', 't', 'q', 1, 2, 3]
-
-const shuffled = unshuffled
-  .map((a) => ({sort: Math.random(), value: a}))
-  .sort((a, b) => a.sort - b.sort)
-  .map((a) => a.value)
-
-console.log(shuffled)
